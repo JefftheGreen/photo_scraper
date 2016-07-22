74ac766e794a301003fb898258c5aa99f4475be0
jeffthegreen <jeff.andrew.collins@gmail.com>
2016-07-22 14:50:52 -0700

    Made Tag-Word relationship one-to-one Forced Tag and Word to be unique based on their content Added method to Tag to create Words Added Source foreign key to Photo Added methods to Photo to create from Tumblr api and to create tags.

--- scraping/models/photos.py
+++ scraping/models/photos.py
@@ -7,10 +7,13 @@ from datetime import datetime as dt
 from tumblpy import Tumblpy
 from tumblpy.exceptions import TumblpyError
 from api_keys import TUMBLR as TUMBLR_KEYS
+import scraping.models.sources as sources
 
 
 class Photo(models.Model):
 
+    # The source the photo came from
+    source = models.ForeignKey(sources.Source)
     # The url of the photo post or page.
     post_url = models.URLField(max_length=200, default='')
     # The url of the photo itself.
@@ -33,19 +36,78 @@ class Photo(models.Model):
     # A rating between 0 and 5 given by the user
     rating = models.IntegerField(default=0)
 
+    # Returns instances based on information pulled from the Tumblr API
+    # (tumblpy). Does not save to database.
+    #   post (str):
+    #       the post, a dictionary derived from tumblpy.
+    #       Must have notes info.
+    #       Via e.g. t.get('posts',
+    #                      'blogname',
+    #                      params={'notes_info': True})['posts'][0]
+    #   source (Source):
+    #       the source the post is derived from. must have been saved to the db.
+    # Returns an array of dictionaries. Each dict is of the form
+    # {'photo': Photo, 'raw tags': tags} where tags is an array of strings.
+    # Tags must be created and saved after saving the Photo instance to the db.
+    @classmethod
+    def from_tumblr_api(cls, post, source):
+        photos = []
+        if post['type'] != 'photo':
+            return photos
+        post_url = post['post_url']
+        posted = tz.make_aware(dt.fromtimestamp(post['timestamp']))
+        post_caption = post['caption']
+        tags = post['tags']
+        likes = len([n for n in post['notes'] if n['type'] == 'like'])
+        for photo in post['photos']:
+            # Create Photo
+            instance = cls()
+            # Set instance variables
+            instance.post_url = post_url
+            instance.photo_url = photo['original_size']['url']
+            instance.posted = posted
+            # Single photos don't have individual captions, use post caption
+            if photo['caption'] != '':
+                instance.caption = photo['caption']
+            else:
+                instance.caption = post_caption
+            instance.likes = likes
+            photos.append({'photo': photo, 'raw tags': tags})
+        return photos
+
+    # Creates, saves, and assigns Tag instances to self.tags
+    def tags_from_ary(self, tags):
+        for raw_tag in tags:
+            if Tag.objects.filter(tag=raw_tag).exists():
+                tag = Tag.objects.get(tag=raw_tag)
+            else:
+                tag = Tag(tag=raw_tag)
+                tag.save()
+                tag.make_word()
+            self.tags.add(tag)
+
 
 class Tag(models.Model):
 
-    tag = models.CharField(max_length=50)
-    word = models.ForeignKey('word', default=None)
+    tag = models.CharField(max_length=50, unique=True)
+    word = models.OneToOneField('word', default=None)
 
     def __str__(self):
         return self.tag
 
+    def make_word(self):
+        if self.word:
+            pass
+        else:
+            word = Word(word=self.tag)
+            word.save()
+            self.word = word
+            self.save()
+
 
 class Word(models.Model):
 
-    word = models.CharField(max_length=50)
+    word = models.CharField(max_length=50, unique=True)
 
     def __str__(self):
         return self.word
